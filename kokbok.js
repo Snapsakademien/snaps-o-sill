2eba95e5db2dcbe99955d55ad371e38955336b68
olbo98 <ob655088@gmail.com>
2019-12-16 23:10:45 +0100

    changed css

--- kokbok.js
+++ kokbok.js
@@ -2,7 +2,17 @@ var recept = [{name: "B1 Special",
              group: "B1", 
              desc: "Den här schnapsen är vidrig", 
              ingredients: "Brännvin, Surströmming", 
-             imgUrl: "#"}
+             imgUrl: "Moth.jpg"},
+              {name: "Test", 
+             group: "T1", 
+             desc: "Test", 
+             ingredients: "Test", 
+             imgUrl: "meme.jpg"},
+              {name: "Test", 
+             group: "T1", 
+             desc: "Test", 
+             ingredients: "Test", 
+             imgUrl: "meme.jpg"}
            ];
 
 
@@ -20,7 +30,7 @@ function createCard(recept){
             break;
     }
     
-    var htmlStr = "<div class='card'><div class='image'></div><div class='footer'><p style='margin: 0'><b>"+recept.name+"</b> <i style='color: "+recept.color+"'>"+recept.group+"</i></p><p class='description'>"+recept.desc+"</p><p style='margin-top: 2rem'>Ingredienser: "+recept.ingredients+"</p></div></div>";
+    var htmlStr = "<div class='card'><div class='image' style='background-image: url("+recept.imgUrl+"); background-size: cover;'></div><div class='footer'><p class='cardHeader'><b>"+recept.name+"</b> <i style='color: "+color+"'>"+recept.group+"</i></p><p class='description'>"+recept.desc+"</p><p style='margin-top: 2rem'>Ingredienser: "+recept.ingredients+"</p></div></div>";
     
     
     wrapper[0].insertAdjacentHTML("beforeend", htmlStr);
