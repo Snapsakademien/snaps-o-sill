65dd692fe85b327831e28e0dee8ec9159eef7672
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-12-26 15:36:27 +0100

    Update placeholder images

--- recept.js
+++ recept.js
@@ -5,35 +5,35 @@ var recept = [
     desc:
       "Den här snapsen kan användas som biologisk krigsföring. Rekommenderas ej.",
     ingredients: "Surströmming",
-    imgUrl: "Moth.jpg"
+    imgUrl: "images/snaps.jpg"
   },
   {
     name: "Test",
     group: "T1",
     desc: "Test",
     ingredients: "Test",
-    imgUrl: "meme.jpg"
+    imgUrl: "images/sill.jpg"
   },
   {
     name: "Test",
     group: "T1",
     desc: "Test",
     ingredients: "Test",
-    imgUrl: "meme.jpg"
+    imgUrl: "images/snaps.jpg"
   },
   {
     name: "Test",
     group: "T1",
     desc: "Test",
     ingredients: "Test",
-    imgUrl: "meme.jpg"
+    imgUrl: "images/snaps.jpg"
   },
   {
     name: "Test",
     group: "T1",
     desc: "Test",
     ingredients: "Test",
-    imgUrl: "meme.jpg"
+    imgUrl: "images/snaps.jpg"
   },
   {
     name: "Test",
@@ -53,6 +53,6 @@ var recept = [
     Where can I get some?
     There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`,
     ingredients: "Test",
-    imgUrl: "meme.jpg"
+    imgUrl: "images/snaps.jpg"
   }
 ];
